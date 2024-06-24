b0b43b1b345f485a1822ed2a3923c9b54e735175
Hugo Fica <hugo.fica.saez@gmail.com>
2024-06-23 23:19:33 -0400

    controladores y rutas para proyectos

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express'
-import { projectGet, projectPost } from '../controllers/project.controllers.js'
+import {
+  projectDelete,
+  projectGet,
+  projectPost
+} from '../controllers/project.controllers.js'
 import { fieldValidation } from '../middlewares/field-validation.js'
 
 export const projectRt = Router()
 
-projectRt.get('/projects', projectGet)
+projectRt.get('/projects/:user_id', projectGet)
 projectRt.post('/newProject', [fieldValidation], projectPost)
+projectRt.delete('/deleteProject/:id/:user_id', projectDelete)
